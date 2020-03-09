0f428b07eafaf89d364125edc88b81a5f9f03e7f
Hunter Evanoff <hunter547@gmail.com>
2020-03-09 18:01:00 -0400

    Try to make the icon appear

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-todo-app',
+    assetsPublicPath: '/vue-todo-app/',
 
     /**
      * Source Maps
